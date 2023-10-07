9d223a4fd394ab1bc2bccb2a6a232c4235889734
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-10-07 11:10:14 +0500

    setting up the server

--- server/app.js
+++ server/app.js
@@ -8,3 +8,20 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
+app.use("/test", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+        path: "config/.env",
+    });
+}
+
+app.use(ErrorHandler);
+
+module.exports = app;
