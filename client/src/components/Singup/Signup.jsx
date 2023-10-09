affda86f65612059b66ddf20a8cbc9962c166ba1
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-10-09 20:54:54 +0500

    backend completed

--- client/src/components/Singup/Signup.jsx
+++ client/src/components/Singup/Signup.jsx
@@ -4,20 +4,32 @@ import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { RxAvatar } from "react-icons/rx";
 import axios from "axios";
-
+import { server } from "../../server";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
   const [avatar, setAvatar] = useState(null);
-  console.log(avatar?.name);
+  console.log(avatar);
   const handleFileChange=(e)=>{
   const file=e.target.files[0]
-  console.log(file);   
   setAvatar(file)
-    // console.log(reader.readAsDataURL());
-    // reader.readAsDataURL(e.target.files[0]);
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config={headers:'Content-Type:multipart/form-data'}
+    const newForm= new FormData()
+    newForm.append('file',avatar)
+    newForm.append('name',name)
+    newForm.append('email',email)
+    newForm.append('password',password)
+    axios.post(`${server}/user/create-user`,newForm,config).then((res)=>{
+      console.log(res);
+    }).catch((error)=>{
+      console.log(error);
+    })
   }
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -28,7 +40,7 @@ const Signup = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
